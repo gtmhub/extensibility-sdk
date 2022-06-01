c33232706fc5787f81d375254b192dfc06175303
Iliya Hanev <85289325+iliyagtmhub@users.noreply.github.com>
2022-06-01 14:00:19 +0300

    Added system level settings (#9)
    Co-authored-by: Ventsislav Visarionov <ventsislav.visarionov@gtmhub.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import axios, { AxiosRequestHeaders } from "axios";
 
 type EventType = "linkIssue" | "getCurrentItem" | "updateCurrentItem" | GeneralEvents | PluginEvents | MetricEvents | GoalEvents | TaskEvents | SessionEvents | UserEvents | AccountEvents | TeamEvents;
-type MetricEvents = "getMetrics" | "getMetric" | "createMetric" | "updateMetric" | "deleteMetric" | "checkinMetric";
-type GoalEvents = "getGoal" | "getGoals" | "createGoal" | "updateGoal" | "deleteGoal";
+type MetricEvents = "getMetrics" | "getMetric" | "createMetric" | "updateMetric" | "deleteMetric" | "checkinMetric" | "patchMetricComment" | "createMetricReaction" | "deleteMetricReaction" | "deleteMetricSnapshot" | "onMetricUpdated";
+type GoalEvents = "getGoal" | "getGoals" | "createGoal" | "updateGoal" | "deleteGoal" | "onGoalUpdated";
 type TaskEvents = "getTask" | "getTasks" | "createTask" | "updateTask" | "deleteTask";
 type UserEvents = "getUser" | "getUsers" | "getCurrentUser" | "createUser" | "updateUser" | "deleteUser";
-type AccountEvents = "getAccountId";
+type AccountEvents = "getAccountId" | "getAccountUrl" | "getAccountSettings" | "getCustomFields";
 type TeamEvents = "getTeam" | "getTeams" | "createTeam" | "updateTeam" | "deleteTeam";
 type PluginEvents = "getSetting" | "getSettings";
 type SessionEvents = "getSession";
-type GeneralEvents = "getDc" | "resize";
+type GeneralEvents = "getDc" | "resize" | "getAssigneeById" | "getAssigneesByIds";
 type Setting = { key: string; value: string };
 
 type dc = "us" | "eu" | "staging";
@@ -24,6 +24,10 @@ class Gtmhub {
     };
   } = {};
 
+  callbackMap: {
+    [method: string]: (payload) => {};
+  } = {};
+
   constructor({ pluginId }) {
     this.pluginId = pluginId;
 
@@ -33,6 +37,14 @@ class Gtmhub {
 
     window.addEventListener("message", (event) => {
       const { type, data } = event.data;
+      const allowedEventTypes = ["onGoalUpdated","onMetricUpdated"];
+
+      // Notify plugin when objective is updated
+      if (allowedEventTypes.indexOf(type) > -1) { 
+        const eventCallback = this.callbackMap[type];
+        eventCallback && eventCallback(data);
+        return;
+      }
 
       // messages can come from different origins, so make sure
       // we use only expected ones
@@ -56,6 +68,13 @@ class Gtmhub {
   createMetric = (payload): Promise<unknown> => this.postMessage("createMetric", { payload });
   updateMetric = (payload): Promise<unknown> => this.postMessage("updateMetric", { payload });
   checkinMetric = (payload): Promise<unknown> => this.postMessage("checkinMetric", { payload });
+  patchMetricComment = (payload): Promise<unknown> => this.postMessage("patchMetricComment", { payload });
+  createMetricReaction = (payload): Promise<unknown> => this.postMessage("createMetricReaction", { payload });
+  deleteMetricReaction = (payload): Promise<unknown> => this.postMessage("deleteMetricReaction", { payload });
+  deleteMetricSnapshot = (payload): Promise<unknown> => this.postMessage("deleteMetricSnapshot", { payload });
+  onMetricUpdated = (cb: (payload) => {}) => {
+    this.callbackMap["onMetricUpdated"] = cb;
+  };
 
   /** GOALS */
   getGoal = (id: string): Promise<unknown> => this.postMessage("getGoal", { id });
@@ -63,6 +82,9 @@ class Gtmhub {
   deleteGoal = (id: string): Promise<unknown> => this.postMessage("deleteGoal", { id });
   createGoal = (payload): Promise<unknown> => this.postMessage("createGoal", { payload });
   updateGoal = (payload): Promise<unknown> => this.postMessage("updateGoal", { payload });
+  onGoalUpdated = (cb: (payload) => {}) => {
+    this.callbackMap["onGoalUpdated"] = cb;
+  };
 
   /** TASKS */
   getTask = (id: string): Promise<unknown> => this.postMessage("getTask", { id });
@@ -98,6 +120,11 @@ class Gtmhub {
   linkIssue = (issue): Promise<unknown> => this.postMessage("linkIssue", issue);
 
   getAccountId = (): Promise<string> => this.postMessage("getAccountId");
+  getAccountUrl = (): Promise<string> => this.postMessage("getAccountUrl");
+  getAccountSettings = (): Promise<unknown> => this.postMessage("getAccountSettings");
+  getAssigneeById = (id): Promise<unknown> => this.postMessage("getAssigneeById", { id });
+  getAssigneesByIds = (payload): Promise<unknown> => this.postMessage("getAssigneesByIds", { payload });
+  getCustomFields = (): Promise<unknown> => this.postMessage("getCustomFields");
 
   resize = (payload: { height: number }): Promise<string> => this.postMessage("resize", { payload });
 
