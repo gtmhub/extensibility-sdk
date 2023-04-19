98cde36a001d20f05f2effd3053c687b93980dfe
Iliya Hanev <iliya@gtmhub.com>
2023-04-19 12:18:50 +0300

    Added axios withCredentials config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import axios, { AxiosRequestHeaders } from "axios";
-import {
-  SdkEventType,
-  GtmhubSdkDc,
-  Setting,
-  sdkEventTypes,
-} from "./models";
+import { SdkEventType, GtmhubSdkDc, Setting, sdkEventTypes } from "./models";
 const sdkVersion = require("../package.json").version;
 
 const DEFAULT_GTMUB_SDK_DC = "eu";
@@ -36,10 +31,7 @@ class Gtmhub {
         return;
       }
 
-      const shouldNotifyPluginOnEventTypes = [
-        "onGoalUpdated",
-        "onMetricUpdated",
-      ];
+      const shouldNotifyPluginOnEventTypes = ["onGoalUpdated", "onMetricUpdated"];
       if (shouldNotifyPluginOnEventTypes.indexOf(type) > -1) {
         const eventCallback = this.callbackMap[type];
         eventCallback && eventCallback(data);
@@ -72,153 +64,111 @@ class Gtmhub {
   }
 
   /** METRICS */
-  getMetric = (id: string): Promise<unknown> =>
-    this.postMessage("getMetric", { id });
-  getMetrics = (id: string): Promise<unknown> =>
-    this.postMessage("getMetrics", { id });
-  deleteMetric = (id: string): Promise<unknown> =>
-    this.postMessage("deleteMetric", { id });
-  createMetric = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createMetric", { payload });
-  updateMetric = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("updateMetric", { payload });
-  checkinMetric = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("checkinMetric", { payload });
-  patchMetricComment = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("patchMetricComment", { payload });
-  createMetricReaction = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createMetricReaction", { payload });
-  deleteMetricReaction = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("deleteMetricReaction", { payload });
-  deleteMetricSnapshot = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("deleteMetricSnapshot", { payload });
+  getMetric = (id: string): Promise<unknown> => this.postMessage("getMetric", { id });
+  getMetrics = (id: string): Promise<unknown> => this.postMessage("getMetrics", { id });
+  deleteMetric = (id: string): Promise<unknown> => this.postMessage("deleteMetric", { id });
+  createMetric = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createMetric", { payload });
+  updateMetric = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("updateMetric", { payload });
+  checkinMetric = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("checkinMetric", { payload });
+  patchMetricComment = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("patchMetricComment", { payload });
+  createMetricReaction = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createMetricReaction", { payload });
+  deleteMetricReaction = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("deleteMetricReaction", { payload });
+  deleteMetricSnapshot = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("deleteMetricSnapshot", { payload });
   onMetricUpdated = (cb: (payload: Record<string, unknown>) => {}) => {
     this.callbackMap["onMetricUpdated"] = cb;
   };
 
   /** GOALS */
-  getGoal = (id: string): Promise<unknown> =>
-    this.postMessage("getGoal", { id });
-  getGoals = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("getGoals", { payload });
-  deleteGoal = (id: string): Promise<unknown> =>
-    this.postMessage("deleteGoal", { id });
-  createGoal = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createGoal", { payload });
-  updateGoal = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("updateGoal", { payload });
+  getGoal = (id: string): Promise<unknown> => this.postMessage("getGoal", { id });
+  getGoals = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("getGoals", { payload });
+  deleteGoal = (id: string): Promise<unknown> => this.postMessage("deleteGoal", { id });
+  createGoal = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createGoal", { payload });
+  updateGoal = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("updateGoal", { payload });
   onGoalUpdated = (cb: (payload: Record<string, unknown>) => {}) => {
     this.callbackMap["onGoalUpdated"] = cb;
   };
 
   /** TASKS */
-  getTask = (id: string): Promise<unknown> =>
-    this.postMessage("getTask", { id });
-  getTasks = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("getTasks", { payload });
-  deleteTask = (id: string): Promise<unknown> =>
-    this.postMessage("deleteTask", { id });
-  createTask = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createTask", { payload });
-  updateTask = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("updateTask", { payload });
+  getTask = (id: string): Promise<unknown> => this.postMessage("getTask", { id });
+  getTasks = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("getTasks", { payload });
+  deleteTask = (id: string): Promise<unknown> => this.postMessage("deleteTask", { id });
+  createTask = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createTask", { payload });
+  updateTask = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("updateTask", { payload });
 
   /** USERS */
-  getUser = (id: string): Promise<unknown> =>
-    this.postMessage("getUser", { id });
-  getUsers = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("getUsers", { payload });
+  getUser = (id: string): Promise<unknown> => this.postMessage("getUser", { id });
+  getUsers = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("getUsers", { payload });
   getCurrentUser = (): Promise<unknown> => this.postMessage("getCurrentUser");
-  deleteUser = (id: string): Promise<unknown> =>
-    this.postMessage("deleteUser", { id });
-  createUser = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createUser", { payload });
-  updateUser = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("updateUser", { payload });
+  deleteUser = (id: string): Promise<unknown> => this.postMessage("deleteUser", { id });
+  createUser = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createUser", { payload });
+  updateUser = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("updateUser", { payload });
 
   /** TEAMS */
-  getTeam = (id: string): Promise<unknown> =>
-    this.postMessage("getTeam", { id });
-  getTeams = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("getTeams", { payload });
-  deleteTeam = (id: string): Promise<unknown> =>
-    this.postMessage("deleteTeam", { id });
-  createTeam = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("createTeam", { payload });
-  updateTeam = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("updateTeam", { payload });
+  getTeam = (id: string): Promise<unknown> => this.postMessage("getTeam", { id });
+  getTeams = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("getTeams", { payload });
+  deleteTeam = (id: string): Promise<unknown> => this.postMessage("deleteTeam", { id });
+  createTeam = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("createTeam", { payload });
+  updateTeam = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("updateTeam", { payload });
 
   /** SESSIONS */
-  getSession = (id: string): Promise<unknown> =>
-    this.postMessage("getSession", { id });
+  getSession = (id: string): Promise<unknown> => this.postMessage("getSession", { id });
 
   /** SETTINGS */
   getSettings = () => this.postMessage<Setting[]>("getSettings");
   getSetting = (id: string) => this.postMessage<Setting>("getSetting", { id });
 
-  updateCurrentItem = (data: unknown): Promise<unknown> =>
-    this.postMessage("updateCurrentItem", data);
+  updateCurrentItem = (data: unknown): Promise<unknown> => this.postMessage("updateCurrentItem", data);
   getCurrentItem = (): Promise<unknown> => this.postMessage("getCurrentItem");
   linkIssue = (issue): Promise<unknown> => this.postMessage("linkIssue", issue);
 
   getAccountId = (): Promise<string> => this.postMessage("getAccountId");
   getAccountUrl = (): Promise<string> => this.postMessage("getAccountUrl");
-  getAccountSettings = (): Promise<unknown> =>
-    this.postMessage("getAccountSettings");
-  getAssigneeById = (id: string): Promise<unknown> =>
-    this.postMessage("getAssigneeById", { id });
-  getAssigneesByIds = (payload: Record<string, unknown>): Promise<unknown> =>
-    this.postMessage("getAssigneesByIds", { payload });
+  getAccountSettings = (): Promise<unknown> => this.postMessage("getAccountSettings");
+  getAssigneeById = (id: string): Promise<unknown> => this.postMessage("getAssigneeById", { id });
+  getAssigneesByIds = (payload: Record<string, unknown>): Promise<unknown> => this.postMessage("getAssigneesByIds", { payload });
   getCustomFields = (): Promise<unknown> => this.postMessage("getCustomFields");
 
-    /** CUSTOM SDK ERRORS */
-    onEventTypeNotSupported = (cb: (payload: Record<string, unknown>) => {}) => {
-      this.callbackMap["onEventTypeNotSupported"] = cb;
-    };
-    onEventTypeNotExpected = (cb: (payload: Record<string, unknown>) => {}) => {
-      this.callbackMap["onEventTypeNotExpected"] = cb;
-    };
-  
-    private notifyMessageTypeNotSupported = (messageType: string) => {
-      const payload = {
-        messageType,
-        sdkVersion,
-      };
-  
-      const green = "\x1b[32m";
-      const blue = "\x1b[34m";
-      const reset = "\x1b[0m";
-      const notSupportedMsg = `Event with name '${blue}${messageType}${reset}' is not supported in the current ${blue}${sdkVersion}${reset} SDK version, please upgrade SDK to the latest one.`;
-      console.log(`${green}Gtmhub SDK:${reset} ${notSupportedMsg}`);
-  
-      const eventCallback = this.callbackMap["onEventTypeNotSupported"];
-      eventCallback && eventCallback(payload);
-    };
-    private notifyMessageTypeNotExpected = (messageType: string) => {
-      const payload = {
-        messageType,
-      };
-  
-      const green = "\x1b[32m";
-      const blue = "\x1b[34m";
-      const reset = "\x1b[0m";
-      const notSupportedMsg = `Unexpected event with name '${blue}${messageType}${reset}'`;
-      console.log(`${green}Gtmhub SDK:${reset} ${notSupportedMsg}`);
-  
-      const eventCallback = this.callbackMap["onEventTypeNotExpected"];
-      eventCallback && eventCallback(payload);
+  /** CUSTOM SDK ERRORS */
+  onEventTypeNotSupported = (cb: (payload: Record<string, unknown>) => {}) => {
+    this.callbackMap["onEventTypeNotSupported"] = cb;
+  };
+  onEventTypeNotExpected = (cb: (payload: Record<string, unknown>) => {}) => {
+    this.callbackMap["onEventTypeNotExpected"] = cb;
+  };
+
+  private notifyMessageTypeNotSupported = (messageType: string) => {
+    const payload = {
+      messageType,
+      sdkVersion,
     };
 
-  resize = (payload: { height: number }): Promise<string> =>
-    this.postMessage("resize", { payload });
+    const green = "\x1b[32m";
+    const blue = "\x1b[34m";
+    const reset = "\x1b[0m";
+    const notSupportedMsg = `Event with name '${blue}${messageType}${reset}' is not supported in the current ${blue}${sdkVersion}${reset} SDK version, please upgrade SDK to the latest one.`;
+    console.log(`${green}Gtmhub SDK:${reset} ${notSupportedMsg}`);
 
-  request = (options: {
-    url: string;
-    method: "GET" | "POST" | "DELETE" | "PUT" | "PATCH";
-    params?: Record<string, unknown>;
-    data?: unknown;
-    headers?: AxiosRequestHeaders;
-  }): Promise<unknown> => {
+    const eventCallback = this.callbackMap["onEventTypeNotSupported"];
+    eventCallback && eventCallback(payload);
+  };
+  private notifyMessageTypeNotExpected = (messageType: string) => {
+    const payload = {
+      messageType,
+    };
+
+    const green = "\x1b[32m";
+    const blue = "\x1b[34m";
+    const reset = "\x1b[0m";
+    const notSupportedMsg = `Unexpected event with name '${blue}${messageType}${reset}'`;
+    console.log(`${green}Gtmhub SDK:${reset} ${notSupportedMsg}`);
+
+    const eventCallback = this.callbackMap["onEventTypeNotExpected"];
+    eventCallback && eventCallback(payload);
+  };
+
+  resize = (payload: { height: number }): Promise<string> => this.postMessage("resize", { payload });
+
+  request = (options: { url: string; method: "GET" | "POST" | "DELETE" | "PUT" | "PATCH"; params?: Record<string, unknown>; data?: unknown; headers?: AxiosRequestHeaders }): Promise<unknown> => {
     const urlObject = new URL(options.url);
     const newUrl = getProxyUrl(this.dc) + urlObject.pathname;
 
@@ -232,6 +182,7 @@ class Gtmhub {
         host: `${urlObject.protocol}//${urlObject.host}`,
         ...options.params,
       },
+      withCredentials: true,
     });
   };
 
